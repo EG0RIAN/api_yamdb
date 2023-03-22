a9d207d4e742cc680fb55c151b21c618cc73951a
Григорий Кононихин <grishakono@gmail.com>
2023-03-23 01:57:30 +0300

    test comment

---
+++ api_yamdb/api/views.py
@@ -0,0 +1,17 @@
+from rest_framework import viewsets
+
+from api.serializers import CategorySerializer, GenreSerializer
+from reviews.models import Category, Genre
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+#new comment
