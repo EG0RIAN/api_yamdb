ad3db5cb1d206d4dc6f811e203050f5cde212765
unknown <demchakromka@gmail.com>
2023-03-28 22:42:03 +0300

    develop

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,10 +21,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'rest_framework',
     'users.apps.UsersConfig',
     'reviews.apps.ReviewsConfig',
-  
     'api'
 ]
 
