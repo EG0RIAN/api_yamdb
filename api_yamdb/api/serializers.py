9b5a8458760b13eb49f7bbabc9ca4a3ab4f290a7
unknown <demchakromka@gmail.com>
2023-03-29 19:38:47 +0300

    исправлен сериализатор Комент и Ревью

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -21,12 +21,16 @@ class GenreSerializer(serializers.ModelSerializer):
 
 
 class ReviewSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username')
     class Meta:
         model = Review
         fields = '__all__'
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username')
     class Meta:
         model = Comment
         fields = '__all__'
